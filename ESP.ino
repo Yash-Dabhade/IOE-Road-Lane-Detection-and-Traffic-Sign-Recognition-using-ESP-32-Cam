717209ff09cd942240f6cf439868edbcaed20a81
YASH DABHADE <64470535+Yash-Dabhade@users.noreply.github.com>
2024-04-05 09:26:36 +0530

    Create ESP.ino

---
+++ ESP.ino
@@ -0,0 +1,55 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <esp32cam.h>
+const char* WIFI_SSID = "******";
+const char* WIFI_PASS = "*****";
+WebServer server(80);
+static auto hiRes = esp32cam::Resolution::find(640, 480);
+void serveJpg()
+{
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    server.send(503, "", "");
+    return;
+  }
+  static_cast<int>(frame->size());
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
+}
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+void  setup(){
+  Serial.begin(115200);
+  Serial.println();
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+    bool ok = Camera.begin(cfg);
+  }
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/cam-hi.jpg");
+  server.on("/cam-hi.jpg", handleJpgHi);
+  server.begin();
+}
+void loop()
+{
+  server.handleClient();
+}
